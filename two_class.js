728144c9db7d24a5786123822f1e1eeb31a92ca9
Luiz <luiz_f_maria@estudante.sesisenai.org.br>
2024-03-27 09:02:58 -0300

    Player e Enemy

--- two_class.js
+++ two_class.js
@@ -1,20 +1,55 @@
-class Obj{
-    constructor(x,y,w,h,a){
-        this.x = x
-        this.y = y
-        this.w = w
-        this.h = h
-        this.a = a
+class Player extends Obj{
+    dir = 0
+    pts = 0
+    vida = 5
+
+    mov(){
+        this.x += this.dir
+        if(this.x <= 0){
+            this.x = 0
+        }else if(this.x >= 835){
+            this.x = 835
+        }
     }
-    des_obj(){
-        
+    
+}
+
+class Inimigo extends Obj{
+    vida_inimigo = 5
+    pararY = 180  // posição onde o inimigo vai parar
+    tempoProximoTiro = Math.random() * 100 // tempo até o próximo tiro do inimigo
+    intervaloTiro = 60 // intervalo de tempo entre os tiros dos inimigos
+    tempoTiro = 0
+
+    atual_inimigo(){
+        if(this.y <= this.pararY){
+            this.y += 0.5
+        }else if(this.vida_inimigo <= 0){
+            this.recomeca()
+        }
+
+        this.tempoTiro++
+        if(this.tempoTiro >= this.tempoProximoTiro){
+            this.tempoTiro = 0
+            this.atira()
+            this.tempoProximoTiro = Math.random() * 350 // define um novo tempo para o próximo tiro
+        }
+    }
+    recomeca(){
+        this.y = -100
+        this.x = Math.floor(Math.random() * ((950 - 2 + 1) + 2)) // quando o inimigo morrer
     }
 
-    des_img(){
-       
+    vel = Math.random() * (2 - 0.5) + 0.5
+
+    mov(){
+        if(this.y <= this.pararY){
+            this.y += 0.5
+        }
+
     }
 
-    colid(objeto) {
-       
+    atira(){
+        grupoTirosInimigo.push(new TiroInimigo(this.x - 40 + this.w, this.y + 40, 30, 40, './assets/tiro.png')) 
     }
 }
